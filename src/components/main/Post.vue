90c7673a86149afd31535177e81c78f6f1ad69a4
leemirae <416homin@daum.net>
2024-02-27 16:36:15 +0900

    feat/#5

---
+++ src/components/main/Post.vue
@@ -0,0 +1,20 @@
+<template>
+  <NotionRenderer :blockMap="blockMap" fullPage />
+</template>
+
+<script setup>
+import { NotionRenderer, getPageBlocks } from 'vue-notion'
+import { ref } from 'vue'
+
+const blockMap = ref(null)
+
+async function create() {
+  blockMap.value = await getPageBlocks('c333c89395ef48b190af92d71da8a3b0')
+}
+
+create()
+</script>
+
+<style>
+@import 'vue-notion/src/styles.css';
+</style>
---
+++ src/components/main/SiteMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <component :is="comp[name]" />
+    <button @click="name = 'List'">게시글 목록</button>
+    <button @click="name = 'Post'">게시글</button>
+  </main>
+</template>
+
+<script setup>
+import { defineAsyncComponent, ref } from 'vue'
+
+const List = defineAsyncComponent(() => import('@/components/main/List.vue'))
+const Post = defineAsyncComponent(() => import('@/components/main/Post.vue'))
+
+const comp = { List, Post }
+const name = ref('')
+</script>
+
+<style>
+main {
+  border: 1px solid red;
+  width: 62vw;
+  margin: auto;
+}
+</style>
